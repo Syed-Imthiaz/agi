87b9a5e69173b4c903a1f3bf13cfab0a302a8ba7
Syed Imthiaz I <syedimthiaz2006@gmail.com>
2026-07-25 13:53:24 +0530

    app
    Fix import statements and initialize Flask app.

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
+import os , re , urlib.request , urlib
+from Flask import flask , jsonify , request , Request
 
+app = flask(__name__)
+req = urllib.request(f"http://www.youtube.com/search?" methods = ("agents", Mozilla/s.0))
+re = urll.b 
